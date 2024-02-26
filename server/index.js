3616f49920e7d480b19aaad7ece965e56f1518c1
Louis Xu <louisxu2003@gmail.com>
2024-02-26 08:59:38 -0500

    testing on ec2

--- server/index.js
+++ server/index.js
@@ -16,11 +16,11 @@ function addDays(date, days) {
   let testDate = new Date("Mar 4, 2024");
   let testTime = 8;
   // console.log(testDate.toString());
-  // await reserveWeekBefore.reserveTest(testDate, testTime);
+  await reserveWeekBefore.reserveTest(testDate, testTime);
   // await reserveWeekBefore.reserve(testDate, testTime);
 
   //Job
-  await reserveWeekBefore.reserve(targetDay, 13);
+  // await reserveWeekBefore.reserve(targetDay, 13);
 
   console.log(`\n##### Job ended at ${new Date()} #####\n`);
 })();
