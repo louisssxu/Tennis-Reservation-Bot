29884e9d3b8eb0cfc93b2637996de39068314aa7
Louis Xu <louisxu2003@gmail.com>
2024-03-10 09:08:20 -0400

    lowered loging delay

--- server/pageActions/loginPopup.js
+++ server/pageActions/loginPopup.js
@@ -2,8 +2,6 @@ const delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
 const { screenshot } = require("./screenshot");
 
 async function login(page) {
-  console.log(process.env.ACC_USER);
-  console.log(process.env.ACC_PASS);
   console.log("Logging in via iframe");
   const frame = await page.waitForSelector(
     "#overlay-container > iframe.overlay-element.overlay-active"
@@ -25,7 +23,7 @@ async function login(page) {
   await screenshot(page, "login");
   const loginSelector = "#login-submit";
   await contentFrame.waitForSelector(loginSelector);
-  await delay(2000);
+  await delay(1000);
   await contentFrame.click(loginSelector);
   await delay(2000);
 }
